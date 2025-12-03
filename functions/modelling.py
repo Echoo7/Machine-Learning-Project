c3e1de962e8c334c98d0c1633c993e26dc88c376
Echoo7 <nathandambreville7@gmail.com>
2025-12-03 19:39:53 +0100

    Merging + correction of clustering

--- functions/modelling.py
+++ functions/modelling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 def my_train_test_split(X, Y, rate = 0.8):
@@ -25,3 +26,11 @@ def build_validation_serie(y_test, y_pred):
         'MAE': mean_absolute_error(y_test, y_pred),
     })
     return s_validation
+
+def haversine(lon1, lat1, lon2, lat2):
+    R = 6371  # Radius of the Earth in kilometers
+    dlon = np.radians(lon2 - lon1)
+    dlat = np.radians(lat2 - lat1)
+    a = np.sin(dlat / 2) ** 2 + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return R * c * 1000  # Return distance in METERS
